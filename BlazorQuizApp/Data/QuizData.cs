d05997ad258be6b792fb967c454eae0ce06f6e01
NIKOLAOS PSALTAKHS <nickpsal@hotmail.com>
2024-01-19 19:10:55 +0200

    Add project files.

---
+++ BlazorQuizApp/Data/QuizData.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorQuizApp.Data
+{
+    public class QuizData
+    {
+        public required string question { get; set; }
+        public required List<string> Answers { get; set; }
+        public required string correct { get; set; }
+    }
+}
